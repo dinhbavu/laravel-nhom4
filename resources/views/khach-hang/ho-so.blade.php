2fc3b1f2f70db9b5036abe7ace1e344996dd2e05
dinhbavu <20221568@eaut.edu.vn>
2026-04-14 16:29:15 +0700

    Truly remove logic files from GitHub

--- resources/views/khach-hang/ho-so.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.khach-hang')
-@section('title', 'Hồ Sơ - VietGo')
-@section('content')
-<div class="container py-8 text-center">
-    <p>Đang chuyển hướng...</p>
-</div>
-@endsection
